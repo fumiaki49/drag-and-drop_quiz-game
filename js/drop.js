7f9f272d0bb81d72c68388c1c2404f2fc7bc127f
fumiaki49 <me.fk39@gmail.com>
2022-10-01 13:07:46 +0900

    delete scoring function

--- js/drop.js
+++ js/drop.js
@@ -39,27 +39,5 @@ $(function () {
 		e.target.classList.add("drop-activated");
 	});
 
-
-	// scoring
-	const scoringBtn = document.getElementById("scoring-btn");
-	scoringBtn.addEventListener("click", event=> {
-		const correctAnswer = document.querySelectorAll('[data-name="true"]');
-		console.log(correctAnswer);
-
-		const uncorrectAnswer = document.querySelectorAll('[data-name="false"]');
-		console.log(uncorrectAnswer);
-
-		const blankAnswer = document.querySelectorAll('[data-name="blank"]');
-		console.log(blankAnswer);
-
-		Swal.fire({
-			title: "採点結果",
-			html: 
-			"<div class = 'score-box'>" + "<span class='score-text'>正解:</span>" + "<span class ='points'>" + correctAnswer.length + "</span>" + "</div>" +
-			"<div class = 'score-box'>" + "<span class='score-text'>不正解:</span>" + "<span class = 'points'>" + uncorrectAnswer.length + "</span>" + "</div>" +
-			"<div class = 'score-box'>" + "<span class='score-text'>空白:</span>" +  "<span class = 'points'>" + blankAnswer.length + "</span>" + "</div>",
-		});
-
-	});
 });
 
