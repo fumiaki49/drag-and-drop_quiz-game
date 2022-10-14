c65204ae422fb2997b106b74776707ce6e9af17e
fumiaki49 <me.fk39@gmail.com>
2022-10-14 14:34:44 +0900

    create index.js

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(function () {
+
+  const headerElement = $("header");
+  $(window).resize(function() {
+    const windowSize = $(this).width();
+    console.log($(this).width());
+  
+    if(windowSize < 768) {
+      headerElement.removeClass("side-header");
+      headerElement.addClass("nav-header");
+    } else if(windowSize > 1024) {
+      headerElement.removeClass("nav-header");
+      headerElement.addClass("side-header");
+    }
+  });
+
+
+  const hambergerBtn = $(".hamberger");
+  const menuList = $(".quiz-list");
+  const hambergerIcon = $(".hamberger .fa-bars");
+
+  $(hambergerBtn).on("click", function() {
+    $(menuList).toggleClass('is-active');
+    $(hambergerIcon).toggleClass('fa-x');
+  });
+});
