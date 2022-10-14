03a61a31809784c35d4aa9bbdd73fcebd2b7d11e
fumiaki49 <me.fk39@gmail.com>
2022-10-14 17:18:06 +0900

    記述の変更

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 $(function () {
 
   const headerElement = $("header");
-  $(window).resize(function() {
-    const windowSize = $(this).width();
-    console.log($(this).width());
-  
-    if(windowSize < 768) {
-      headerElement.removeClass("side-header");
-      headerElement.addClass("nav-header");
-    } else if(windowSize > 1024) {
-      headerElement.removeClass("nav-header");
-      headerElement.addClass("side-header");
-    }
-  });
+  // $(window).resize(function() {
+  // });
+  const windowSize = $(window).width();
+
+  if(windowSize <= 768) {
+    headerElement.removeClass("side-header");
+    headerElement.addClass("nav-header");
+  } else if(windowSize >= 1024) {
+    headerElement.removeClass("nav-header");
+    headerElement.addClass("side-header");
+  }
 
 
   const hambergerBtn = $(".hamberger");
