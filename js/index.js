47fb0bdc00ade38a771816286aa722b079f53264
fumiaki49 <me.fk39@gmail.com>
2022-10-15 10:36:52 +0900

    記述の変更

--- js/index.js
+++ js/index.js
@@ -1,25 +1,30 @@
 $(function () {
 
-  const headerElement = $("header");
-  // $(window).resize(function() {
-  // });
-  const windowSize = $(window).width();
+  function judgeWindowSize() {
+    const headerElement = $("header");
+    const windowSize = $(window).width();
+  
+    if(windowSize <= 768) {
+      headerElement.removeClass("side-header");
+      headerElement.addClass("nav-header");
+    } else if(windowSize >= 1024) {
+      headerElement.removeClass("nav-header");
+      headerElement.addClass("side-header");
+    }
+  };
+  judgeWindowSize();
 
-  if(windowSize <= 768) {
-    headerElement.removeClass("side-header");
-    headerElement.addClass("nav-header");
-  } else if(windowSize >= 1024) {
-    headerElement.removeClass("nav-header");
-    headerElement.addClass("side-header");
-  }
 
-
-  const hambergerBtn = $(".hamberger");
-  const menuList = $(".quiz-list");
-  const hambergerIcon = $(".hamberger .fa-bars");
-
-  $(hambergerBtn).on("click", function() {
-    $(menuList).toggleClass('is-active');
-    $(hambergerIcon).toggleClass('fa-x');
-  });
+  function hambergerMenu() {
+    const hambergerBtn = $(".hamberger");
+    const menuList = $(".quiz-list");
+    const hambergerIcon = $(".hamberger .fa-bars");
+  
+    $(hambergerBtn).on("click", function() {
+      $(menuList).toggleClass('is-active');
+      $(hambergerIcon).toggleClass('fa-x');
+    });
+  };
+  hambergerMenu();
+  
 });
