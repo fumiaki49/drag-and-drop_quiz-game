5147757c99af5021201217790c9cde454238fe72
fumiaki49 <me.fk39@gmail.com>
2022-10-25 13:26:10 +0900

    不要な記述を削除

--- js/barba.js
+++ js/barba.js
@@ -1,24 +1,5 @@
 'use strict';
-
 const mask = document.querySelector(".mask");
-
-const eventDelete = e => {
-  if (e.currentTarget.href === window.location.href) {
-    e.preventDefault()
-    e.stopPropagation()
-    return
-  }
-}
-const links = [...document.querySelectorAll("a[href]")];
-links.forEach(link => {
-  link.addEventListener(
-    "click",
-    e => {
-      eventDelete(e);
-    },
-    false
-  );
-});
 
 const windowSize = window.innerWidth;
 if(windowSize >= 1024) {
