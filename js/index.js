1e725902c0cbcb307ab3d7f748b08645d2849e7a
fumiaki49 <me.fk39@gmail.com>
2022-10-25 13:26:27 +0900

    記述の変更

--- js/index.js
+++ js/index.js
@@ -1,34 +1,40 @@
 $(function () {
-  function judgeWindowSize() {
-    const headerElement = $("header");
-    const windowSize = $(window).width();
-    
-    if(windowSize <= 768) {
-      headerElement.removeClass("side-header");
-      headerElement.addClass("nav-header");
-    } else if(windowSize >= 1024) {
-      headerElement.removeClass("nav-header");
-      headerElement.addClass("side-header");
-    }
-  };
-  judgeWindowSize();
-  
   function hambergerMenu() {
-    const hambergerBtn = $(".hamberger");
-    const hambergerIcon = $(".hamberger .fa-bars");
-    const quizList = $(".quiz-list");
-    const quizLink = $(".accordion-list li");
-
-    $(hambergerBtn).on("click", function() {
-      $(quizList).toggleClass('is-active');
-      $(hambergerIcon).toggleClass('fa-x');
-    });
-    
-    $(quizLink).on("click", function() {
+    $(document).on("click", ".hamberger, .accordion-list li", function() {
+      const hambergerIcon = $(".hamberger .fa-bars");
+      const quizList = $(".quiz-list");
       $(quizList).toggleClass('is-active');
       $(hambergerIcon).toggleClass('fa-x');
     });
   };
   hambergerMenu();
+
+  function scoringFunction() {
+    $(document).on('click', '#scoring-btn', function () {
+      const correctAnswer = document.querySelectorAll('[data-name="true"]');
+      const uncorrectAnswer = document.querySelectorAll('[data-name="false"]');
+      const blankAnswer = document.querySelectorAll('[data-name="blank"]');
+      const quizList = document.querySelectorAll(".drop");
+    
+      if ( uncorrectAnswer.length > 0 || blankAnswer.length > 0 ) {
+        Swal.fire({
+          title: "もう一度考えてみよう！",
+          text: "空白 or 不正解の箇所があります" 
+        })
+      
+      } else if( correctAnswer.length == quizList.length ) {
+        Swal.fire({
+          title: "全問正解!",
+          text: "別の問題にも挑戦してみよう！",
+        });
+    
+        party.confetti(this, {
+          lifetime: party.variation.range(2, 4),
+          count: 100,
+        });
+      };
+    });
+  };
+  scoringFunction();
 
 });
